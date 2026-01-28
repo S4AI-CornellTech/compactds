68276fbec9b9a5927d318a69b998c8a0a82d682c
michaeltshen <mts247@cornell.edu>
2026-01-28 15:32:19 -0500

    More dynamicism again

--- retrieval_nq.sh
+++ retrieval_nq.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: bash $0 \"nprobe_list\" \"n_docs_list\""
-  echo "Example: bash $0 \"1 32 64 128 256 512\" \"5 10 20 50\""
+if [ "$#" -ne 4 ]; then
+  echo "Usage: bash $0 compactds_base_path output_dir \"nprobe_list\" \"n_docs_list\""
+  echo "Example: bash $0 /path/to/compactds /path/to/output_dir \"1 32 64 128 256 512\" \"5 10 20 50\""
   exit 1
 fi
 
-read -a nprobe_list <<< "$1"
-read -a n_docs_list <<< "$2"
+compactds_base_path="$1"
+output_dir="$2"
+read -a nprobe_list <<< "$3"
+read -a n_docs_list <<< "$4"
 
 for p in "${nprobe_list[@]}"; do
   for n_docs in "${n_docs_list[@]}"; do
     python -m src.main_ric --config-name CompactDS \
       tasks.eval.search=true \
-      datastore.embedding.passages_dir=datastores/compactds/passages \
-      datastore.embedding.embedding_dir=datastores/compactds/embeddings \
+      datastore.embedding.passages_dir="$compactds_base_path/passages" \
+      datastore.embedding.embedding_dir="$compactds_base_path/embeddings" \
       tasks.eval.task_name=lm-eval \
       evaluation.data.eval_data=queries/naturalqs::olmes_q.jsonl \
-      evaluation.eval_output_dir=/share/suh-scrap/mts247/rag-selector/docids/compactds \
+      evaluation.eval_output_dir="$output_dir" \
       evaluation.search.n_docs=$n_docs \
       datastore.index.probe=$p
   done
